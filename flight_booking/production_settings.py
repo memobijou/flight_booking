58f8f6ae3f8b6be6e6817a80586af2472fab3798
Mimoun Bijjou <mimounbijjou@Mimouns-Air.fritz.box>
2019-08-07 13:52:39 +0200

    initial commit

---
+++ flight_booking/production_settings.py
@@ -0,0 +1,12 @@
+from flight_booking.settings import *
+import dj_database_url
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL
+    )
+}
+
+SECURE_SSL_REDIRECT = True
