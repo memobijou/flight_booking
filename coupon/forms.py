0a7966487b410aa600fbe448fd5768f07047ee33
Mimoun Bijjou <mimounbijjou@Mimouns-Air.fritz.box>
2019-09-15 00:57:21 +0200

    prüfnummer hover

--- coupon/forms.py
+++ coupon/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 
 
+coupon_amounts = [100, 150, 200, 250, 300, 350, 400, 450, 500]
+
+
 class CouponForm(forms.Form):
     coupon_amount = forms.IntegerField(required=True)
     stripe_token = forms.CharField(required=True)
+
+    def clean_coupon_amount(self):
+        data = self.cleaned_data.get("coupon_amount")
+        if data not in coupon_amounts:
+            self.add_error("coupon_amount", "Ungültiger Gutschein.")
+        return data
