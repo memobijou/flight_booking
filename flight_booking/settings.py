97b0a69e38e0a00319ad5f267f2e3ad7f8ea86a7
Mimoun Bijjou <mimounbijjou@Mimouns-Air.local>
2019-09-16 14:40:32 +0200

    port smtp

--- flight_booking/settings.py
+++ flight_booking/settings.py
@@ -155,7 +155,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 # if os.environ.get("EMAIL_HOST_USER"):
 #     _, mail_domain = os.environ.get("EMAIL_HOST_USER").split("@")
 #     DEFAULT_FROM_EMAIL = "no-reply" + "@" + mail_domain
